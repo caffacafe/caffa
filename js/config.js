da9121bc5fb3b1806e09c53d53c4f8d6f9096a9f
caffacafe <zakiadel643@gmail.com>
2026-05-09 20:51:15 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,8 +11,7 @@
 
 // ⚙️ إعدادات Supabase - مطعم النخبة
 window.SUPABASE_URL = 'sb_publishable_UKSgwgwwY2-rHOYA-Fu-Pw_YzwMCdqY';
-window.SUPABASE_KEY = 'sb_secret_bjA2eXNsNQnAA43QQjymiA_CK3qSvCx';
-
+window.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3bmt1c3hwbGhzZWx0b3lvZXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMzcwMjIsImV4cCI6MjA5MzkxMzAyMn0.fUJHuLG0K8jgvO9ZwD1MPpjk-qSmcwQxNs-6AJEmqF0';
 // مثال:
 // window.SUPABASE_URL = 'https://abcdefghij.supabase.co';
 // window.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
